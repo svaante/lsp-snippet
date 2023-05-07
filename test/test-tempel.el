3a471e4ca1487e33bef2a2347928eace4cc99638
Daniel Pettersson <daniel@dpettersson.net>
2023-05-07 13:57:02 +0200

    Update tests

--- test/test-tempel.el
+++ test/test-tempel.el
@@ -3,8 +3,7 @@
 (ert-deftest lsp-mode-snippet-tempel-parsing-test ()
   (lsp-snippet-tempel-lsp-mode-init)
   (with-temp-buffer
-    (insert "printf($1)$0")
+    (insert "printf(${1:some_thing})$0")
     (lsp-snippet-tempel--expand-snippet nil (point-min) (point-max) nil)
     (should (equal (substring-no-properties (buffer-string))
-                   "printf()"))
-    ))
+                   "printf(some_thing)"))))
